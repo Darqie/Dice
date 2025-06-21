64d56a8f8064cdf87fc13b11365bc756c9773819
Darqie <ilevsheny@gmail.com>
2025-06-21 18:21:44 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 import Stack from "@mui/material/Stack";
 import Container from "@mui/material/Container";
-import { useEffect } from "react";
 import OBR from "@owlbear-rodeo/sdk";
 
 import { InteractiveTray } from "./tray/InteractiveTray";
 import { Sidebar } from "./controls/Sidebar";
 
 export function App() {
-  useEffect(() => {
-    const setupMetadataListener = () => {
-      const handleMetadataChange = async (metadata: any) => {
-        // Видаляємо обробку запиту з App.tsx - вона буде тільки в DiceRollSync.tsx
-        // Це запобігає подвійному кидку
-      };
-
-      const unsubscribe = OBR.room.onMetadataChange(handleMetadataChange);
-      
-      return () => {
-        unsubscribe();
-      };
-    };
-    
-    OBR.onReady(() => {
-      setupMetadataListener();
-    });
-  }, []);
-
   return (
     <Container disableGutters maxWidth="md">
       <Stack direction="row" justifyContent="center">
